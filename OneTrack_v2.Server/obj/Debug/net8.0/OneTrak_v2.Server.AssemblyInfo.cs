fe30298825a4b431cc70b11586742a3c795f3151
ErishMF <erish.faggett.ce@omf.com>
2024-02-22 14:46:44 -0600

    - ignore update

--- OneTrack_v2.Server/obj/Debug/net8.0/OneTrak_v2.Server.AssemblyInfo.cs
+++ OneTrack_v2.Server/obj/Debug/net8.0/OneTrak_v2.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe1d6fc3c2c23f09524f45ea0c0e593adc8d4f9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874ed456b8b7fecfa9a22ebec6d9247cdc1b1d16")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
