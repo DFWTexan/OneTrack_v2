9ddb6b127937a49e4498f4cee37b582ab09c4b89
ErishMF <erish.faggett.ce@omf.com>
2024-02-29 15:30:10 -0600

    CLIENT - Modal displaying for TM Information...

--- onetrack_v2.client/src/app/team-member/agent-information/tm-information/tm-information.component.ts
+++ onetrack_v2.client/src/app/team-member/agent-information/tm-information/tm-information.component.ts
@@ -1,14 +1,50 @@
-import { Component, OnInit, Injectable } from '@angular/core';
+import { Component, OnInit, Injectable, OnDestroy } from '@angular/core';
+
+import { AgentDataService } from '../../../_services';
+import { Subscription } from 'rxjs';
+import { AgentInfo } from '../../../_Models';
+import { ModalService } from '../../../_services';
 
 @Component({
   selector: 'app-tm-information',
   templateUrl: './tm-information.component.html',
-  styleUrl: './tm-information.component.css'
+  styleUrl: './tm-information.component.css',
 })
 @Injectable()
-export class TmInformationComponent implements OnInit {
-  constructor() {}
+export class TmInformationComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  agentInfo: AgentInfo = {} as AgentInfo;
+  bodyText = 'This text can be updated in modal 1';
 
-  ngOnInit() {}
+  constructor(
+    private agentService: AgentDataService,
+    protected modalService: ModalService
+  ) {
+    this.subscription = new Subscription();
+  }
 
+  ngOnInit() {
+    this.subscription = this.agentService.agentInfoChanged.subscribe(
+      (agentInfo: any) => {
+        this.agentInfo = agentInfo;
+      }
+    );
+  }
+
+  openModal() {
+    const modalDiv = document.getElementById('myModal');
+    if (modalDiv != null) {
+      modalDiv.style.display = 'block';
+    }
+  }
+  closeModal() {
+    const modalDiv = document.getElementById('myModal');
+    if (modalDiv != null) {
+      modalDiv.style.display = 'none';
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
