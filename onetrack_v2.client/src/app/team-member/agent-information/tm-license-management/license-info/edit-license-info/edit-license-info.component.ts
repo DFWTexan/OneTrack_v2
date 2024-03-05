3e09700d49d6bba972bf5f389d3d594cbd639378
ErishMF <erish.faggett.ce@omf.com>
2024-03-05 16:24:47 -0600

    CLIENT - Edit Lincense Info Setup
    - First input set for Edit-License-Info...

--- onetrack_v2.client/src/app/team-member/agent-information/tm-license-management/license-info/edit-license-info/edit-license-info.component.ts
+++ onetrack_v2.client/src/app/team-member/agent-information/tm-license-management/license-info/edit-license-info/edit-license-info.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-license-info',
   templateUrl: './edit-license-info.component.html',
   styleUrl: './edit-license-info.component.css'
 })
-export class EditLicenseInfoComponent {
+export class EditLicenseInfoComponent implements OnInit {
+  licenseForm: FormGroup;
+  
+  constructor(private fb: FormBuilder) {
+    this.licenseForm = this.fb.group({
+      employeeLicenseId: [{value: '', disabled: true}, Validators.required],
+      licenseState: ['', Validators.required],
+      lineOfAuthority: ['', Validators.required],
+      licenseStatus: ['', Validators.required],
+      employmentID: ['', Validators.required],
+      licenseName: ['', Validators.required],
+      licenseNumber: ['', Validators.required],
+      resNoneRes: [''],
+      originalIssueDate: ['', Validators.required],
+      lineOfAuthIssueDate: ['', Validators.required],
+      licenseEffectiveDate: ['', Validators.required],
+      licenseExpirationDate: ['', Validators.required],
+    });
+  }
 
+  ngOnInit(): void {}
 }
