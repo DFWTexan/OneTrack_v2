59409673b4962df4f623fc0fa9b6619961750067
ErishMF <erish.faggett.ce@omf.com>
2024-03-05 22:07:38 -0600

    CLIENT - Populating Edit-License-Info

--- onetrack_v2.client/src/app/team-member/agent-information/tm-license-management/license-info/edit-license-info/edit-license-info.component.ts
+++ onetrack_v2.client/src/app/team-member/agent-information/tm-license-management/license-info/edit-license-info/edit-license-info.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { AgentDataService } from '../../../../../_services';
+import { AgentLicenseAppointments } from '../../../../../_Models';
+
 @Component({
   selector: 'app-edit-license-info',
   templateUrl: './edit-license-info.component.html',
-  styleUrl: './edit-license-info.component.css'
+  styleUrl: './edit-license-info.component.css',
 })
 export class EditLicenseInfoComponent implements OnInit {
   licenseForm: FormGroup;
-  
-  constructor(private fb: FormBuilder) {
+  currentIndex: number = 0;
+  licenseMgmtData: AgentLicenseAppointments[] = [];
+
+  constructor(
+    private agentDataService: AgentDataService,
+    private fb: FormBuilder
+  ) {
     this.licenseForm = this.fb.group({
-      employeeLicenseId: [{value: '', disabled: true}, Validators.required],
+      employeeLicenseId: [{ value: '', disabled: true }, Validators.required],
       licenseState: ['', Validators.required],
       lineOfAuthority: ['', Validators.required],
       licenseStatus: ['', Validators.required],
@@ -26,5 +34,23 @@ export class EditLicenseInfoComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.currentIndex = this.agentDataService.licenseMgmtDataIndex;
+    this.licenseMgmtData =
+      this.agentDataService.agentInformation.agentLicenseAppointments;
+      this.licenseForm.patchValue({
+        employeeLicenseId: this.licenseMgmtData[this.currentIndex].employeeLicenseId,
+        licenseState: 'MO',
+        lineOfAuthority: 'CL',
+        licenseStatus: 'Active',
+        employmentID: '606709',
+        licenseName: 'CREDIT',
+        licenseNumber: '3002117601',
+        resNoneRes: null,
+        originalIssueDate: '2022-08-30T00:00:00',
+        lineOfAuthIssueDate: '2022-08-30T00:00:00',
+        licenseEffectiveDate: '2022-08-30T00:00:00',
+        licenseExpirationDate: '2025-05-30T00:00:00',
+      });
+  }
 }
