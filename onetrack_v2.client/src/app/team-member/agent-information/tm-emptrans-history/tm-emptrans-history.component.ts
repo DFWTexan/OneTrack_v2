0503948e489f9295f375a607c2ef6d17fa9b7ee9
ErishMF <erish.faggett.ce@omf.com>
2024-03-07 13:00:34 -0600

    CLIENT - Employment Transfer History
    - Setup Framework for Layout...

--- onetrack_v2.client/src/app/team-member/agent-information/tm-emptrans-history/tm-emptrans-history.component.ts
+++ onetrack_v2.client/src/app/team-member/agent-information/tm-emptrans-history/tm-emptrans-history.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AgentInfo } from '../../../_Models';
+import { AgentDataService, ModalService } from '../../../_services';
 
 @Component({
   selector: 'app-tm-emptrans-history',
   templateUrl: './tm-emptrans-history.component.html',
   styleUrl: './tm-emptrans-history.component.css'
 })
-export class TmEmptransHistoryComponent {
+@Injectable()
+export class TmEmptransHistoryComponent implements OnInit, OnDestroy {
+  agentInfo: AgentInfo = {} as AgentInfo;
+  subscribeAgentInfo: Subscription;
+  
+  constructor(
+    private agentService: AgentDataService,
+    protected modalService: ModalService
+  ) {
+    this.subscribeAgentInfo = new Subscription();
+  }
 
+  ngOnInit(): void {
+    this.subscribeAgentInfo = this.agentService.agentInfoChanged.subscribe(
+      (agentInfo: any) => {
+        this.agentInfo = agentInfo;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscribeAgentInfo.unsubscribe();
+  }
 }
