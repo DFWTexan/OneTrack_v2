bed5367577d96b2c61ad933f2310414a79ab3cb0
ErishMF <erish.faggett.ce@omf.com>
2024-03-08 00:38:16 -0600

    CLIENT - styling...

--- OneTrack_v2.Server/obj/Debug/net8.0/OneTrak_v2.Server.AssemblyInfo.cs
+++ OneTrack_v2.Server/obj/Debug/net8.0/OneTrak_v2.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196a1c8a87c40b14e1fce6a9b1872b3e783a53ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f72154cabb6ae7436f97138973b8bf510f1cab04")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
