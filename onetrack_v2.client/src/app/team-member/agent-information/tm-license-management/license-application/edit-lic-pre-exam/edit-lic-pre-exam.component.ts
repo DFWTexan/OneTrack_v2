532483997bd06ab966c599fee8dd754c7f9eca38
ErishMF <erish.faggett.ce@omf.com>
2024-03-13 11:40:59 -0500

    CLIENT -- License Pre Exam
    - Complete EDIT for PreExam Form...

--- onetrack_v2.client/src/app/team-member/agent-information/tm-license-management/license-application/edit-lic-pre-exam/edit-lic-pre-exam.component.ts
+++ onetrack_v2.client/src/app/team-member/agent-information/tm-license-management/license-application/edit-lic-pre-exam/edit-lic-pre-exam.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { formatDate } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { AgentComService, AgentDataService } from '../../../../../_services';
 
 @Component({
   selector: 'app-edit-lic-pre-exam',
   templateUrl: './edit-lic-pre-exam.component.html',
-  styleUrl: './edit-lic-pre-exam.component.css'
+  styleUrl: './edit-lic-pre-exam.component.css',
 })
-export class EditLicPreExamComponent {
+@Injectable()
+export class EditLicPreExamComponent implements OnInit, OnDestroy {
+  licPreExamForm!: FormGroup;
+  subscriptionMode: Subscription = new Subscription();
+  subscriptionData: Subscription = new Subscription();
 
+  constructor(
+    public agentDataService: AgentDataService,
+    public agentComService: AgentComService
+  ) {}
+
+  ngOnInit(): void {
+    this.licPreExamForm = new FormGroup({
+      employeeLicensePreExamID: new FormControl({
+        value: '',
+        disabled: true,
+      }),
+      employeeLicenseID: new FormControl(null),
+      status: new FormControl(null),
+      examID: new FormControl(null),
+      examName: new FormControl(null),
+      examScheduleDate: new FormControl(null),
+      examTakenDate: new FormControl(null),
+      additionalNotes: new FormControl(null),
+    });
+
+    this.subscriptionMode =
+      this.agentComService.modeLicPreExamChanged.subscribe((mode: string) => {
+        if (mode === 'EDIT') {
+          this.subscriptionData =
+            this.agentDataService.licensePreExamItemChanged.subscribe(
+              (licPreExam: any) => {
+                this.licPreExamForm.patchValue({
+                  employeeLicensePreExamID: licPreExam.employeeLicensePreExamID,
+                  employeeLicenseID: licPreExam.employeeLicenseID,
+                  status: licPreExam.status,
+                  examID: licPreExam.examID,
+                  examName: licPreExam.examName,
+                  examScheduleDate: formatDate(
+                    licPreExam.examScheduleDate,
+                    'yyyy-MM-dd',
+                    'en-US'
+                  ),
+                  examTakenDate: formatDate(
+                    licPreExam.examTakenDate,
+                    'yyyy-MM-dd',
+                    'en-US'
+                  ),
+                  additionalNotes: licPreExam.additionalNotes,
+                });
+              }
+            );
+        } else {
+          this.licPreExamForm.reset();
+        }
+      });
+  }
+
+  onSubmit(): void { }
+
+  ngOnDestroy(): void {
+    this.subscriptionMode.unsubscribe();
+    this.subscriptionData.unsubscribe();
+  }
 }
