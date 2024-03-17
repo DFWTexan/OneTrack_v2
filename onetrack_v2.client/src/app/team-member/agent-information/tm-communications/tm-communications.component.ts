ead7c7396f0b7010b1f7cbc7fd08e06842e08260
ErishMF <erish.faggett.ce@omf.com>
2024-03-17 08:53:26 -0500

    CLIENT - TM Communication
    - Completed TM Communication tab...

--- onetrack_v2.client/src/app/team-member/agent-information/tm-communications/tm-communications.component.ts
+++ onetrack_v2.client/src/app/team-member/agent-information/tm-communications/tm-communications.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AgentComService, AgentDataService, ModalService } from '../../../_services';
+import { AgentInfo } from '../../../_Models';
 
 @Component({
   selector: 'app-tm-communications',
   templateUrl: './tm-communications.component.html',
   styleUrl: './tm-communications.component.css'
 })
-export class TmCommunicationsComponent {
+@Injectable()
+export class TmCommunicationsComponent implements OnInit, OnDestroy {
+  agentInfo: AgentInfo = {} as AgentInfo;
+  subscribeAgentInfo: Subscription;
 
+  constructor(
+    public agentDataService: AgentDataService,
+    public agentComService: AgentComService,
+    protected modalService: ModalService
+  ) {
+    this.subscribeAgentInfo = new Subscription();
+  }
+
+  ngOnInit(): void {
+    this.subscribeAgentInfo = this.agentDataService.agentInfoChanged.subscribe(
+      (agentInfo: any) => {
+        this.agentInfo = agentInfo;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscribeAgentInfo.unsubscribe();
+  }
 }
