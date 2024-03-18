1074647c379e1f44699bbfbcd236eb4079b5d9f1
ErishMF <erish.faggett.ce@omf.com>
2024-03-18 14:49:20 -0500

    CLIENT - Email ComTemplates
    - Progress SAVE...

--- onetrack_v2.client/src/app/_services/email/email.data.service.ts
+++ onetrack_v2.client/src/app/_services/email/email.data.service.ts
@@ -5,15 +5,62 @@ import { map } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment';
 import { AgentComService } from '../agent/agentInfo.com.service';
+import { EmailComTemplate } from '../../_Models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmailDataService {
-    private apiUrl: string = environment.apiUrl;
+  private apiUrl: string = environment.apiUrl + 'Email/';
+  emailComTemplates: EmailComTemplate[] = [];
+  emailComTemplatesChanged = new Subject<EmailComTemplate[]>();
+  htmlContent: string = '';
+  htmlContentChanged = new Subject<string>();
 
-    constructor(
-        private http: HttpClient,
-        private agentComService: AgentComService
-      ) {}
+  constructor(
+    private http: HttpClient,
+    private agentComService: AgentComService
+  ) {}
+
+  fetchEmailComTemplates(): Observable<EmailComTemplate[]> {
+    return this.http
+      .get<{
+        success: boolean;
+        statusCode: number;
+        objData: EmailComTemplate[];
+        errMessage: string;
+      }>(this.apiUrl + 'GetEmailComTemplates')
+      .pipe(
+        map((response) => {
+          if (response.success && response.statusCode === 200) {
+            this.emailComTemplates = response.objData;
+            this.emailComTemplatesChanged.next(this.emailComTemplates);
+            return response.objData;
+          } else {
+            throw new Error(response.errMessage || 'Unknown error');
+          }
+        })
+      );
+  }
+
+  fetchEmailComTemplateByID(communicationID: number, employmentID: number): Observable<string> {
+    return this.http
+      .get<{
+        success: boolean;
+        statusCode: number;
+        objData: any;
+        errMessage: string;
+      }>(this.apiUrl + 'GetEmailTemplate/' + communicationID + '/' + employmentID)
+      .pipe(
+        map((response) => {
+          if (response.success && response.statusCode === 200) {
+            this.htmlContent = response.objData;
+            this.htmlContentChanged.next(this.htmlContent);
+            return response.objData;
+          } else {
+            throw new Error(response.errMessage || 'Unknown error');
+          }
+        })
+      );
+  }
 }
--- onetrack_v2.client/src/app/team-member/agent-information/tm-email/tm-email.component.ts
+++ onetrack_v2.client/src/app/team-member/agent-information/tm-email/tm-email.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+import { EmailComTemplate } from '../../../_Models';
+import { EmailDataService } from '../../../_services';
 
 @Component({
   selector: 'app-tm-email',
   templateUrl: './tm-email.component.html',
-  styleUrl: './tm-email.component.css'
+  styleUrl: './tm-email.component.css',
 })
-export class TmEmailComponent {
+@Injectable()
+export class TmEmailComponent implements OnInit, OnDestroy {
+  emailComTemplates: EmailComTemplate[] = [];
+  htmlContent: string = '';
+  subscribeEmailComTemplates: Subscription;
 
+  constructor(private emailDataService: EmailDataService) {
+    this.subscribeEmailComTemplates = new Subscription();
+  }
+
+  ngOnInit(): void {
+    this.emailDataService
+      .fetchEmailComTemplates()
+      .subscribe((emailComTemplates: EmailComTemplate[]) => {
+        this.emailComTemplates = emailComTemplates;
+      });
+  }
 
   onSubmit(form: NgForm) {
     console.log(form);
   }
 
+  ngOnDestroy(): void {
+    this.subscribeEmailComTemplates.unsubscribe();
+  }
 }
