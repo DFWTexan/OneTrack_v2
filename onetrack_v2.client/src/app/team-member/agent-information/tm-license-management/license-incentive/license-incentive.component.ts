44955afd66b09991fb8ddcec86fb7e8648ab47cb
ErishMF <erish.faggett.ce@omf.com>
2024-03-19 13:47:08 -0500

    CLIENT - TM License Incentive
    Progress SAVE ...

--- onetrack_v2.client/src/app/team-member/agent-information/tm-license-management/license-incentive/license-incentive.component.ts
+++ onetrack_v2.client/src/app/team-member/agent-information/tm-license-management/license-incentive/license-incentive.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+
+import {
+  // AgentLicApplicationInfo,
+  AgentLicenseAppointments,
+} from '../../../../_Models';
+import {
+  AgentComService,
+  AgentDataService,
+  ModalService,
+} from '../../../../_services';
 
 @Component({
   selector: 'app-license-incentive',
   templateUrl: './license-incentive.component.html',
-  styleUrl: './license-incentive.component.css'
+  styleUrl: './license-incentive.component.css',
 })
-export class LicenseIncentiveComponent {
+@Injectable()
+export class LicenseIncentiveComponent implements OnInit {
+  licenseMgmtData: AgentLicenseAppointments[] = [];
+  currentIndex: number = 0;
+  panelOpenState = false;
 
+  constructor(
+    public agentDataService: AgentDataService,
+    public agentComService: AgentComService,
+    protected modalService: ModalService
+  ) {}
+
+  ngOnInit(): void {
+    this.currentIndex = this.agentDataService.licenseMgmtDataIndex;
+    this.licenseMgmtData =
+      this.agentDataService.agentInformation.agentLicenseAppointments;
+    this.agentDataService
+      .fetchAgentLicApplicationInfo(
+        this.licenseMgmtData[this.currentIndex].employeeLicenseId
+      )
+  }
+
+  // Pagination
+  nextPage() {
+    if (this.currentIndex < this.licenseMgmtData.length - 1) {
+      this.currentIndex++;
+      this.agentDataService.licenseMgmtDataIndexChanged.next(this.currentIndex);
+    }
+  }
+
+  previousPage() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.agentDataService.licenseMgmtDataIndexChanged.next(this.currentIndex);
+    }
+  }
+
+  getPageInfo(): string {
+    return `${this.currentIndex + 1} of ${this.licenseMgmtData.length}`;
+  }
+
+  isDisplayPrevious(): boolean {
+    return this.currentIndex > 0;
+  }
+
+  isDisplayNext(): boolean {
+    return this.currentIndex < this.licenseMgmtData.length - 1;
+  }
 }
