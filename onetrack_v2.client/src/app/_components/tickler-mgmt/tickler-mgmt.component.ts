47d0bc70c01c3b7c8920d1564e59394d146973ef
ErishMF <erish.faggett.ce@omf.com>
2024-03-23 00:01:21 -0500

    CLIENT - Disabled to not show Search tab on search-members

--- onetrack_v2.client/src/app/_components/tickler-mgmt/tickler-mgmt.component.ts
+++ onetrack_v2.client/src/app/_components/tickler-mgmt/tickler-mgmt.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tickler-mgmt',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
 })
 export class TicklerMgmtComponent {
   
+  constructor(public router: Router) { }
 
 }
