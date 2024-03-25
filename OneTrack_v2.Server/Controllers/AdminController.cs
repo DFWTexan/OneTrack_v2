00fa1d56cef87953ac2ac74b20d38f12972b2e10
ErishMF <erish.faggett.ce@omf.com>
2024-03-25 11:58:49 -0500

    SERVER - AdminController setup...

--- OneTrack_v2.Server/Controllers/AdminController.cs
+++ OneTrack_v2.Server/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OneTrack_v2.DataModel;
 using OneTrack_v2.Services;
+using OneTrak_v2.Services;
 
 namespace OneTrack_v2.Controllers
 {
@@ -9,5 +10,174 @@ namespace OneTrack_v2.Controllers
     [ApiController]
     public class AdminController : ControllerBase
     {
+        
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        #region "Admin Display"
+        [HttpGet]
+        public async Task<IActionResult> GetAgentByEmployeeID()
+        {
+            var result = await Task.Run(() => _adminService.GetCompanyTypes());
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{companyTypeID}")]
+        public async Task<IActionResult> GetAgentByEmployeeID(int companyTypeID)
+        {
+            var result = await Task.Run(() => _adminService.GetCompanyByType(companyTypeID));
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLicenseTypes()
+        {
+            var result = await Task.Run(() => _adminService.GetLicenseTypes());
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{state}/{licenesTypeID}")]
+        public async Task<ActionResult> GetConEduLicenses(string state, int licenesTypeID)
+        {
+            var result = await Task.Run(() => _adminService.GetConEduLicenses(state, licenesTypeID));
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{workState}/{resState}")]
+        public async Task<ActionResult> GetCompanyRequirements(string workState, string resState)
+        {
+            var result = await Task.Run(() => _adminService.GetCompanyRequirements(workState, resState));
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDropdownListTypes()
+        {
+            var result = await Task.Run(() => _adminService.GetDropdownListTypes());
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{lkpField}")]
+        public async Task<IActionResult> GetDropdownByType(string lkpField)
+        {
+            var result = await Task.Run(() => _adminService.GetDropdownByType(lkpField));
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{state}")]
+        public async Task<IActionResult> GetExamByState(string state)
+        {
+            var result = await Task.Run(() => _adminService.GetExamByState(state));
+
+            return StatusCode(result.StatusCode, result);
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetJobTitleLicLevel()
+        {
+            var result = await Task.Run(() => _adminService.GetJobTitleLicLevel());
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetJobTitlelicIncentive()
+        {
+            var result = await Task.Run(() => _adminService.GetJobTitlelicIncentive());
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetJobTitlelicensed()
+        {
+            var result = await Task.Run(() => _adminService.GetJobTitlelicensed());
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLicenseEditList()
+        {
+            var result = await Task.Run(() => _adminService.GetLicenseEditList());
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{licenseID}")]
+        public async Task<IActionResult> GetLicenseEditByID(int licenseID)
+        {
+            var result = await Task.Run(() => _adminService.GetLicenseEditByID(licenseID));
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLicTechList()
+        {
+            var result = await Task.Run(() => _adminService.GetLicTechList());
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{state}")]
+        public async Task<IActionResult> GetPreEduEditByState(string state)
+        {
+            var result = await Task.Run(() => _adminService.GetPreEduEditByState(state));
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductEditList()
+        {
+            var result = await Task.Run(() => _adminService.GetProductEditList());
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{workState}/{resState}")]
+        public async Task<ActionResult> GetStateLicRequirementList(string workState, string resState)
+        {
+            var result = await Task.Run(() => _adminService.GetStateLicRequirementList(workState, resState));
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetStateProvinceList()
+        {
+            var result = await Task.Run(() => _adminService.GetStateProvinceList());
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetXBorderBranchList()
+        {
+            var result = await Task.Run(() => _adminService.GetXBorderBranchList());
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{branchCode}")]
+        public async Task<IActionResult> GetXBorderBranchByCode(int branchCode)
+        {
+            var result = await Task.Run(() => _adminService.GetXBorderBranchByCode(branchCode));
+
+            return StatusCode(result.StatusCode, result);
+        }
+        #endregion
     }
 }
--- OneTrack_v2.Server/obj/Debug/net8.0/OneTrak_v2.Server.AssemblyInfo.cs
+++ OneTrack_v2.Server/obj/Debug/net8.0/OneTrak_v2.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f54590e95541a7ba3c445d0f67289df6c7ceaf1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+011489a09a5fc2afe46679b23dbe583509443cfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
