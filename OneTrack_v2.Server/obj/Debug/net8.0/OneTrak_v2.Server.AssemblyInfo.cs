97fbfb2f8da6fb8e3a2b74b3b42c9dda97478538
ErishMF <erish.faggett.ce@omf.com>
2024-04-08 11:51:02 -0500

    Assembly update

--- OneTrack_v2.Server/obj/Debug/net8.0/OneTrak_v2.Server.AssemblyInfo.cs
+++ OneTrack_v2.Server/obj/Debug/net8.0/OneTrak_v2.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ab8f84c599a8e921fbf5c31b9bd2ee595faf7c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+980912c5bd46583c9d4da7864bf922ede227a75e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
