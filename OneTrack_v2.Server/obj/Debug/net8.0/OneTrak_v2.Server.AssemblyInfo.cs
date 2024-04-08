c0388e5f46fdc9b5cdf4132508c9b9ade6b575f3
ErishMF <erish.faggett.ce@omf.com>
2024-04-08 11:56:40 -0500

    NuGet Updates...

--- OneTrack_v2.Server/obj/Debug/net8.0/OneTrak_v2.Server.AssemblyInfo.cs
+++ OneTrack_v2.Server/obj/Debug/net8.0/OneTrak_v2.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+980912c5bd46583c9d4da7864bf922ede227a75e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97fbfb2f8da6fb8e3a2b74b3b42c9dda97478538")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OneTrak_v2.Server.Test/obj/Debug/net8.0/OneTrak_v2.Server.Test.AssemblyInfo.cs
+++ OneTrak_v2.Server.Test/obj/Debug/net8.0/OneTrak_v2.Server.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneTrak_v2.Server.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ab8f84c599a8e921fbf5c31b9bd2ee595faf7c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97fbfb2f8da6fb8e3a2b74b3b42c9dda97478538")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneTrak_v2.Server.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneTrak_v2.Server.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
