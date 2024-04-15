2c068bb74a39fa95f2e506942f26c7efb655385f
ErishMF <erish.faggett.ce@omf.com>
2024-04-15 13:12:02 -0500

    CLIENT - EDIT License PreExam and PreEdu

--- onetrack_v2.client/src/app/admin/license-tech-edit/edit-license-tech/edit-license-tech.component.ts
+++ onetrack_v2.client/src/app/admin/license-tech-edit/edit-license-tech/edit-license-tech.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+import { AdminComService, AdminDataService } from '../../../_services';
 
 @Component({
   selector: 'app-edit-license-tech',
   templateUrl: './edit-license-tech.component.html',
   styleUrl: './edit-license-tech.component.css'
 })
-export class EditLicenseTechComponent {
+@Injectable()
+export class EditLicenseTechComponent implements OnInit, OnDestroy {
+  licenseTechForm!: FormGroup;
+  subscriptionData: Subscription = new Subscription();
+
+  constructor(
+    public adminDataService: AdminDataService,
+    public adminComService: AdminComService
+  ) {}
+
+  ngOnInit(): void {
+    this.licenseTechForm = new FormGroup({
+      licenseTechId: new FormControl(''),
+      soeid: new FormControl(''),
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      isActive: new FormControl(''),
+      teamNum: new FormControl(''),
+      licenseTechPhone: new FormControl(''),
+      licenseTechFax: new FormControl(''),
+      licenseTechEmail: new FormControl(''),
+      techName: new FormControl(''),
+    });
+
+    this.subscriptionData =
+      this.adminComService.modes.licenseTech.changed.subscribe((mode: string) => {
+        if (mode === 'EDIT') {
+          this.adminDataService.licenseTechChanged.subscribe((licenseTech: any) => {
+            this.licenseTechForm.patchValue({
+              licenseTechId: licenseTech.licenseTechId,
+              soeid: licenseTech.soeid,
+              firstName: licenseTech.firstName,
+              lastName: licenseTech.lastName,
+              isActive: licenseTech.isActive,
+              teamNum: licenseTech.teamNum,
+              licenseTechPhone: licenseTech.licenseTechPhone,
+              licenseTechFax: licenseTech.licenseTechFax,
+              licenseTechEmail: licenseTech.licenseTechEmail,
+              techName: licenseTech.techName,
+            });
+          });
+        } else {
+          this.licenseTechForm.reset();
+        }
+      });
+  }
+
+  onSubmit(): void {}
+
+  ngOnDestroy(): void {
+    this.subscriptionData.unsubscribe();
+  }
 
 }
--- onetrack_v2.client/src/app/admin/pre-education-edit/edit-pre-education/edit-pre-education.component.ts
+++ onetrack_v2.client/src/app/admin/pre-education-edit/edit-pre-education/edit-pre-education.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+import { AdminComService, AdminDataService } from '../../../_services';
 
 @Component({
   selector: 'app-edit-pre-education',
   templateUrl: './edit-pre-education.component.html',
   styleUrl: './edit-pre-education.component.css'
 })
-export class EditPreEducationComponent {
+@Injectable()
+export class EditPreEducationComponent implements OnInit, OnDestroy {
+  preEducationForm!: FormGroup;
+  subscriptionData: Subscription = new Subscription();
 
+  constructor(
+    public adminDataService: AdminDataService,
+    public adminComService: AdminComService
+  ) {}
+
+  ngOnInit(): void {
+    this.preEducationForm = new FormGroup({
+      preEducationId: new FormControl(''),
+      educationName: new FormControl(''),
+      stateProvinceAbv: new FormControl(''),
+      creditHours: new FormControl(''),
+      deliveryMethod: new FormControl(''),
+    });
+
+    this.subscriptionData =
+      this.adminComService.modes.preEducation.changed.subscribe((mode: string) => {
+        if (mode === 'EDIT') {
+          this.adminDataService.preEducationChanged.subscribe((preEducation: any) => {
+            this.preEducationForm.patchValue({
+              preEducationId: preEducation.preEducationId,
+              educationName: preEducation.educationName,
+              stateProvinceAbv: preEducation.stateProvinceAbv,
+              creditHours: preEducation.creditHours,
+              deliveryMethod: preEducation.deliveryMethod,
+            });
+          });
+        } else {
+          this.preEducationForm.reset();
+        }
+      });
+  }
+
+  onSubmit(): void {}
+
+  ngOnDestroy(): void {
+    this.subscriptionData.unsubscribe();
+  }
 }
