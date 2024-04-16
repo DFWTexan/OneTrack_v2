15a13da80eb4c06f552ff2b2367d7347bbbc126e
ErishMF <erish.faggett.ce@omf.com>
2024-04-16 08:53:19 -0500

    CLIENT/Assembly - Going to Reports

--- OneTrack_v2.Server/obj/Debug/net8.0/OneTrak_v2.Server.AssemblyInfo.cs
+++ OneTrack_v2.Server/obj/Debug/net8.0/OneTrak_v2.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75223bcd9ac9dec0211f8c6b67341ac042ff690e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0458629d27f8601c0f100853530a6db57d44130e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneTrak_v2.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
