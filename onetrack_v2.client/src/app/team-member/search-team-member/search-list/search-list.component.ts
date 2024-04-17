20a4fe1d704f443cb8939c0ce7576effa1b2d6a7
ErishMF <erish.faggett.ce@omf.com>
2024-04-17 15:07:42 -0500

    CLIENT - Employee Search Tickler & Indexer for Search reusults

--- onetrack_v2.client/src/app/team-member/search-team-member/search-list/search-list.component.ts
+++ onetrack_v2.client/src/app/team-member/search-team-member/search-list/search-list.component.ts
@@ -3,22 +3,25 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EmployeeSearchResult } from '../../../_Models';
 import { Subscription } from 'rxjs';
 
-import { EmployeeDataService } from '../../../_services';
+import { EmployeeDataService, ModalService } from '../../../_services';
 
 @Component({
   selector: 'app-search-list',
   templateUrl: './search-list.component.html',
   styleUrl: './search-list.component.css',
 })
-export class SearchListComponent implements OnInit, OnDestroy{
+export class SearchListComponent implements OnInit, OnDestroy {
   searchEmployeeResults: EmployeeSearchResult[] = [];
-  subscription: Subscription = new Subscription;
+  subscription: Subscription = new Subscription();
 
-  constructor(private emplyService: EmployeeDataService) {}
+  constructor(
+    private emplyService: EmployeeDataService,
+    public modalService: ModalService
+  ) {}
 
   ngOnInit() {
-    this.subscription = this.emplyService.employeeSearchResultsChanged
-    .subscribe(
+    this.subscription =
+      this.emplyService.employeeSearchResultsChanged.subscribe(
         (employeeSearchResults: EmployeeSearchResult[]) => {
           this.searchEmployeeResults = employeeSearchResults;
         }
