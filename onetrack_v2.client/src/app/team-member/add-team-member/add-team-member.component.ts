34a80d0cf5547d6fa275ce9cbdd28bfb1c8543da
ErishMF <erish.faggett.ce@omf.com>
2024-04-19 14:06:39 -0500

    CLIENT - Add Team Member

--- onetrack_v2.client/src/app/team-member/add-team-member/add-team-member.component.ts
+++ onetrack_v2.client/src/app/team-member/add-team-member/add-team-member.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { zip } from 'rxjs';
 
 @Component({
   selector: 'app-add-team-member',
   templateUrl: './add-team-member.component.html',
-  styleUrl: './add-team-member.component.css'
+  styleUrl: './add-team-member.component.css',
 })
 export class AddTeamMemberComponent {
+  newAgentForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.newAgentForm = this.fb.group({
+      firstName: [''],
+      middleName: [''],
+      lastName: [''],
+      preferedName: [''],
+      dateOfBirth: [''],
+      employeeSSN: [''],
+      teamID: [''],
+      soeid: [''],
+      nationalProducerNumber: [''],
+      employerAgency: [''],
+      workState: [''],
+      resState: [''],
+      jobTitle: [''],
+      hireDate: [''],
+      branchCode: [''],
+      address1: [''],
+      address2: [''],
+      city: [''],
+      zip: [''],
+      email: [''],
+      homePhone: [''],
+      workPhone: [''],
+      fax: [''],
+    });
+  }
+
+  onSubmit() {
+    // Handle form submission
+  }
+
+  onCancel() {
+    // Handle cancel action
+  }
 }
