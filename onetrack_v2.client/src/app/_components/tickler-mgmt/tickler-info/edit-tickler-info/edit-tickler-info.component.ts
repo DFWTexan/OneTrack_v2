0a1813a98504e1d0bba693d2aec2fe9bfe666087
ErishMF <erish.faggett.ce@omf.com>
2024-04-21 15:59:45 -0500

    CLIENT- EDIT Tickler Info

--- onetrack_v2.client/src/app/_components/tickler-mgmt/tickler-info/edit-tickler-info/edit-tickler-info.component.ts
+++ onetrack_v2.client/src/app/_components/tickler-mgmt/tickler-info/edit-tickler-info/edit-tickler-info.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+import {
+  TicklerMgmtComService,
+  TicklerMgmtDataService,
+} from '../../../../_services';
 
 @Component({
   selector: 'app-edit-tickler-info',
   templateUrl: './edit-tickler-info.component.html',
-  styleUrl: './edit-tickler-info.component.css'
+  styleUrl: './edit-tickler-info.component.css',
 })
-export class EditTicklerInfoComponent {
+@Injectable()
+export class EditTicklerInfoComponent implements OnInit, OnDestroy {
+  ticklerForm!: FormGroup;
+  subscriptionData: Subscription = new Subscription();
 
+  constructor(
+    private fb: FormBuilder,
+    public ticklerDataService: TicklerMgmtDataService,
+    public ticklerComService: TicklerMgmtComService
+  ) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.ticklerForm = this.fb.group({
+      ticklerId: [null],
+      ticklerDate: [null],
+      ticklerDueDate: [null],
+      licenseTechId: [null],
+      employmentId: [null],
+      employeeLicenseId: [null],
+      employeeId: [null],
+      ticklerCloseDate: [null],
+      ticklerCloseByLicenseTechId: [null],
+      message: [null],
+      lkpValue: [null],
+    });
+  }
+
+  ngOnInit(): void {
+    this.subscriptionData =
+      this.ticklerComService.modeTicklerMgmtChanged.subscribe(
+        (mode: string) => {
+          if (mode === 'EDIT') {
+            this.ticklerDataService.ticklerInfoChanged.subscribe(
+              (ticklerInfo) => {
+                this.ticklerForm.patchValue({
+                  ticklerId: ticklerInfo.ticklerId,
+                  ticklerDate: ticklerInfo.ticklerDate,
+                  ticklerDueDate: ticklerInfo.ticklerDueDate,
+                  licenseTechId: ticklerInfo.licenseTechId,
+                  employmentId: ticklerInfo.employmentId,
+                  employeeLicenseId: ticklerInfo.employeeLicenseId,
+                  employeeId: ticklerInfo.employeeId,
+                  ticklerCloseDate: ticklerInfo.ticklerCloseDate,
+                  ticklerCloseByLicenseTechId:
+                    ticklerInfo.ticklerCloseByLicenseTechId,
+                  message: ticklerInfo.message,
+                  lkpValue: ticklerInfo.lkpValue,
+                });
+              }
+            );
+          } else {
+            this.ticklerForm.reset();
+          } 
+        }
+      );
+  }
+
+  ngOnDestroy(): void {}
+
+  onSubmit() {
+    console.log(this.ticklerForm.value);
+  }
 }
