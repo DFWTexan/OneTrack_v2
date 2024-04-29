71ee2870dcdab99be0b8a8da5e9787c12d2aa71b
ErishMF <erish.faggett.ce@omf.com>
2024-04-29 10:49:41 -0500

    CLIENT - EDIT Diary Entry

--- onetrack_v2.client/src/app/team-member/agent-information/tm-diary/edit-diary-entry/edit-diary-entry.component.ts
+++ onetrack_v2.client/src/app/team-member/agent-information/tm-diary/edit-diary-entry/edit-diary-entry.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AgentComService, AgentDataService } from '../../../../_services';
 
 @Component({
   selector: 'app-edit-diary-entry',
   templateUrl: './edit-diary-entry.component.html',
-  styleUrl: './edit-diary-entry.component.css'
+  styleUrl: './edit-diary-entry.component.css',
 })
-export class EditDiaryEntryComponent {
+export class EditDiaryEntryComponent implements OnInit, OnDestroy {
+  diaryItemForm!: FormGroup;
+  subscriptionMode: Subscription = new Subscription();
+  subscriptionData: Subscription = new Subscription();
 
+  constructor(
+    public agentService: AgentDataService,
+    public agentComService: AgentComService
+  ) {}
+
+  ngOnInit(): void {
+    this.diaryItemForm = new FormGroup({
+      diaryID:new FormControl({ value: '', disabled: true }),
+      soeid: new FormControl(null),
+      diaryName: new FormControl(null),
+      diaryDate: new FormControl(null),
+      notes: new FormControl(null)
+    });
+
+    this.subscriptionMode =
+      this.agentComService.modeDiaryChanged.subscribe(
+        (mode: string) => {
+          if (mode === 'EDIT') {
+            this.subscriptionData =
+              this.agentService.diaryEntryChanged.subscribe(
+                (diaryItem: any) => {
+                  this.diaryItemForm.patchValue({
+                    diaryID: diaryItem.diaryID,
+                    soeid: diaryItem.soeid,
+                    diaryName: diaryItem.diaryName,
+                    diaryDate: diaryItem.diaryDate,
+                    notes: diaryItem.notes
+                  });
+                }
+              );
+          }
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionMode.unsubscribe();
+    this.subscriptionData.unsubscribe();
+  }
 }
--- onetrack_v2.client/src/app/team-member/agent-information/tm-emptrans-history/edit-jobtitle-hist/edit-jobtitle-hist.component.ts
+++ onetrack_v2.client/src/app/team-member/agent-information/tm-emptrans-history/edit-jobtitle-hist/edit-jobtitle-hist.component.ts
@@ -3,7 +3,12 @@ import { FormGroup, FormControl } from '@angular/forms';
 import { formatDate } from '@angular/common';
 import { Subscription } from 'rxjs';
 
-import { AgentComService, AgentDataService, AppComService, MiscDataService } from '../../../../_services';
+import {
+  AgentComService,
+  AgentDataService,
+  AppComService,
+  MiscDataService,
+} from '../../../../_services';
 
 @Component({
   selector: 'app-edit-jobtitle-hist',
@@ -13,9 +18,9 @@ import { AgentComService, AgentDataService, AppComService, MiscDataService } fro
 @Injectable()
 export class EditJobtitleHistComponent implements OnInit, OnDestroy {
   jobTitleForm!: FormGroup;
-  jobTitleList: Array<{ jobTitleID: number, jobTitle: string }> = [];
-  subscriptionMode: Subscription = new Subscription;
-  subscriptionData: Subscription = new Subscription;
+  jobTitleList: Array<{ jobTitleID: number; jobTitle: string }> = [];
+  subscriptionMode: Subscription = new Subscription();
+  subscriptionData: Subscription = new Subscription();
 
   constructor(
     public agentService: AgentDataService,
@@ -35,46 +40,56 @@ export class EditJobtitleHistComponent implements OnInit, OnDestroy {
       isCurrent: new FormControl(null),
     });
 
-    this.subscriptionMode = this.agentComService.modeEmploymentJobTitleHistChanged.subscribe(
-      (mode: string) => {
-        if (mode === 'EDIT') {
-          this.subscriptionData = this.agentService.employmentJobTitleHistItemChanged.subscribe(
-            (jobTitle: any) => {
-              this.jobTitleForm.patchValue({
-                employmentJobTitleID: jobTitle.employmentJobTitleID,
-                employmentID: jobTitle.employmentID,
-                jobTitleDate: formatDate(
-                  jobTitle.jobTitleDate,
-                  'yyyy-MM-dd',
-                  'en-US'
-                ),
-                jobCode: jobTitle.jobCode,
-                jobTitleID: jobTitle.jobTitleID,
-                jobTitle: jobTitle.jobTitle,
-                isCurrent: jobTitle.isCurrent,
-              });
-            }
-          );
-        } else {
-          this.jobTitleForm.reset();
-          this.jobTitleForm.patchValue({
-            jobTitleID: 0,
-            // jobTitleDate: formatDate(new Date(), 'yyyy-MM-dd', 'en-US'),
-            isCurrent: false,
-          });
+    this.subscriptionMode =
+      this.agentComService.modeEmploymentJobTitleHistChanged.subscribe(
+        (mode: string) => {
+          if (mode === 'EDIT') {
+            this.subscriptionData =
+              this.agentService.employmentJobTitleHistItemChanged.subscribe(
+                (jobTitle: any) => {
+                  this.jobTitleForm.patchValue({
+                    employmentJobTitleID: jobTitle.employmentJobTitleID,
+                    employmentID: jobTitle.employmentID,
+                    jobTitleDate: formatDate(
+                      jobTitle.jobTitleDate,
+                      'yyyy-MM-dd',
+                      'en-US'
+                    ),
+                    jobCode: jobTitle.jobCode,
+                    jobTitleID: jobTitle.jobTitleID,
+                    jobTitle: jobTitle.jobTitle,
+                    isCurrent: jobTitle.isCurrent,
+                  });
+                }
+              );
+          } else {
+            this.jobTitleForm.reset();
+            this.jobTitleForm.patchValue({
+              jobTitleID: 0,
+              // jobTitleDate: formatDate(new Date(), 'yyyy-MM-dd', 'en-US'),
+              isCurrent: false,
+            });
+          }
         }
-      }
-    );
+      );
 
-    this.miscDataService.fetchJobTitles().subscribe(
-      (jobTitles: Array<{ jobTitleID: number, jobTitle: string }>) => {
-        this.jobTitleList = [ { jobTitleID: 0, jobTitle: 'Select' }, ...jobTitles];
-      }
-    );
+    this.miscDataService
+      .fetchJobTitles()
+      .subscribe(
+        (jobTitles: Array<{ jobTitleID: number; jobTitle: string }>) => {
+          this.jobTitleList = [
+            { jobTitleID: 0, jobTitle: 'Select' },
+            ...jobTitles,
+          ];
+        }
+      );
   }
 
   onSubmit() {
-    console.log('EMFTest - (app-edit-jobtitle-hist) onSubmit => \n', this.jobTitleForm.value);
+    console.log(
+      'EMFTest - (app-edit-jobtitle-hist) onSubmit => \n',
+      this.jobTitleForm.value
+    );
   }
 
   ngOnDestroy(): void {
