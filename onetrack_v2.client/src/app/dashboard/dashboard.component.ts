ebdfae204c61e563b274d1194982650a418df8fd
ErishMF <erish.faggett.ce@omf.com>
2024-05-01 13:16:27 -0500

    CLIENT - Dashboard Progress SAVE

--- onetrack_v2.client/src/app/dashboard/dashboard.component.ts
+++ onetrack_v2.client/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,75 @@
-import { Component } from '@angular/core';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+import {
+  DashboardDataService,
+  MiscDataService,
+  WorkListDataService,
+} from '../_services';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, OnDestroy {
+  loading: boolean = false;
   panelOpenState = false;
+
+  worklistData: any[] = [];
+  worklistNames: string[] = ['Loading...'];
+
+  workListName: string = 'Agent Address Change';
+  licenseTech: string = 'T9999999';
+  date: string | null = null;
+
+  selectedWorkListName = 'Agent Address Change';
+  selectedLicenseTech = 'T9999999';
+  selectedDate: string | null;
+
+  constructor(
+    public workListDataService: WorkListDataService,
+    public miscDataService: MiscDataService,
+    public dashboardDataService: DashboardDataService
+  ) {
+    // this.selectedLicenseTech = null;
+    this.selectedDate = null;
+  }
+
+  ngOnInit(): void {
+    this.miscDataService.fetchWorkListNames().subscribe((worklistNames) => {
+      this.worklistNames = worklistNames;
+      this.selectedWorkListName = worklistNames[0];
+    });
+    // this.miscDataService.fetchWorkListNames().subscribe((worklistNames) => {
+    //   this.worklistNames = worklistNames;
+    //   this.selectedWorkListName = worklistNames[0];
+    // });
+    // this.miscDataService.fetchLicenseTechs().subscribe((licenseTechs) => {
+    //   this.licenseTechs = licenseTechs;
+    //   this.selectedLicenseTech = licenseTechs[0].soeid;
+    // });
+    // this.fetchWorkListData();
+  }
+
+  fetchWorkListData(): void {
+    this.workListDataService
+      .fetchWorkListData(
+        this.selectedWorkListName,
+        this.selectedDate,
+        this.selectedLicenseTech
+      )
+      .subscribe((worklistData) => {
+        this.worklistData = worklistData;
+      });
+  }
+
+  changeWorkListName(event: any) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.selectedWorkListName = value;
+    this.fetchWorkListData();
+  }
+
+  ngOnDestroy(): void {}
 }
