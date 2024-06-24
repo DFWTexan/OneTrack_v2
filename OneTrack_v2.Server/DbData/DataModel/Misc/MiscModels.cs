b074a3d820acb134f09b531cf2403ccc34e71a91
ErishMF <erish.faggett.ce@omf.com>
2024-06-24 15:26:12 -0500

    Update MiscModels.cs

--- OneTrack_v2.Server/DbData/DataModel/Misc/MiscModels.cs
+++ OneTrack_v2.Server/DbData/DataModel/Misc/MiscModels.cs
@@ -17,4 +17,12 @@
         public string? Value { get; set; } = null;
         public string? Label { get; set; } = null;
     }
+
+    public class IputFileUpload
+    {
+        public string? FileName { get; set; } = null;
+        public string? FileUrl { get; set; } = null;
+        public FormFile? File { get; set; } = null;
+    }
+    
 }
