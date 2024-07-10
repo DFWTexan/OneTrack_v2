653ab4863ca1d4c9f80f0a122ecca88520f24ab4
ErishMF <erish.faggett.ce@omf.com>
2024-07-10 13:47:16 -0500

    Progress SAVE...

--- onetrack_v2.client/src/app/admin/xbor-lic-requirements/edit-xbor-requirement/edit-xbor-requirement.component.ts
+++ onetrack_v2.client/src/app/admin/xbor-lic-requirements/edit-xbor-requirement/edit-xbor-requirement.component.ts
@@ -2,7 +2,7 @@ import { Component, Injectable, OnInit, OnDestroy, EventEmitter, Output, Input }
 import { FormGroup, FormControl } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
-import { AdminComService, AdminDataService } from '../../../_services';
+import { AdminComService, AdminDataService, DropdownDataService, ErrorMessageService, UserAcctInfoDataService } from '../../../_services';
 
 @Component({
   selector: 'app-edit-xbor-requirement',
@@ -13,20 +13,34 @@ import { AdminComService, AdminDataService } from '../../../_services';
 export class EditXborRequirementComponent implements OnInit, OnDestroy {
   @Output() callParentRefreshData = new EventEmitter<any>();
   @Input() stateProvinces: any[] = [];
+  isFormSubmitted = false;
   xborLicenseRequirementForm!: FormGroup;
+  stateLicenseItems: {value: number, label: string}[] = [];
+  selectedLicenseState: string = 'Select';
+  isLicenseNameDisabled = true;
+  isStartDocUploaded = false;
+  isRenewalDocUploaded = false;
+  FileDisplayMode = 'CHOOSEFILE'; //--> CHOSEFILE / ATTACHMENT
+  file: File | null = null;
+  fileUri: string | null = null;
+  document: string = '';
+
   subscriptionData: Subscription = new Subscription();
 
   constructor(
+    private errorMessageService: ErrorMessageService,
     public adminDataService: AdminDataService,
-    public adminComService: AdminComService
+    public adminComService: AdminComService,
+    public dropDownDataService: DropdownDataService,
+    private userAcctInfoDataService: UserAcctInfoDataService
   ) {}
 
   ngOnInit(): void {
     this.xborLicenseRequirementForm = new FormGroup({
       requiredLicenseId: new FormControl(''),
       workStateAbv: new FormControl(''),
-      resStateAbv: new FormControl(''),
-      licenseId: new FormControl(''),
+      resStateAbv: new FormControl({value: 'Select', disabled: false}),
+      licenseId: new FormControl({value: 0, disabled: true}),
       branchCode: new FormControl(''),
       requirementType: new FormControl(''),
       licLevel1: new FormControl(''),
@@ -37,7 +51,7 @@ export class EditXborRequirementComponent implements OnInit, OnDestroy {
       incentive2_Plus: new FormControl(''),
       licIncentive3: new FormControl(''),
       licState: new FormControl(''),
-      licenseName: new FormControl(''),
+      licenseName: new FormControl({value: 'Select', disabled: true}),
       startDocument: new FormControl(''),
       renewalDocument: new FormControl(''),
     });
@@ -45,7 +59,14 @@ export class EditXborRequirementComponent implements OnInit, OnDestroy {
     this.subscriptionData =
       this.adminComService.modes.xborLicenseRequirement.changed.subscribe((mode: string) => {
         if (mode === 'EDIT') {
+          this.stateProvinces = this.stateProvinces.filter(item => item !== 'Select');
           this.adminDataService.xborLicenseRequirementChanged.subscribe((xborLicenseRequirement: any) => {
+            this.isStartDocUploaded = xborLicenseRequirement.startDocument ? true : false;
+                this.isRenewalDocUploaded = xborLicenseRequirement.renewalDocument ? true : false;
+                if (xborLicenseRequirement.licenseId !== 0) {
+                  this.selectedLicenseState = xborLicenseRequirement.licState;
+                  this.fetchStateLicenseItems();
+                }
             this.xborLicenseRequirementForm.patchValue({
               requiredLicenseId: xborLicenseRequirement.requiredLicenseId,
               workStateAbv: xborLicenseRequirement.workStateAbv,
@@ -72,7 +93,71 @@ export class EditXborRequirementComponent implements OnInit, OnDestroy {
       });
   }
 
-  onSubmit(): void {}
+  onFileSelected(event: any) {
+    const target = event.target as HTMLInputElement;
+    if (target.files && target.files.length) {
+      const file: File = target.files[0];
+
+      if (file) {
+        this.document = file.name;
+        const formData = new FormData();
+        formData.append('thumbnail', file);
+        // const upload$ = this.emailDataService.uploadFile(formData);
+        // upload$.subscribe();
+      }
+    }
+  }
+
+  onChangeLicenseState(event: any) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.selectedLicenseState = value;
+
+    if (value === 'Select') {
+      this.xborLicenseRequirementForm.get('licenseId')?.disable();
+      return;
+    } else {
+      this.xborLicenseRequirementForm.get('licenseId')?.enable(); 
+      this.fetchStateLicenseItems();
+    }
+  }
+
+  onSubmit(): void {
+    this.isFormSubmitted = true;
+    let stateReqItem: any = this.xborLicenseRequirementForm.value;
+    // licenseTechItem.jobTitleID = 0;
+    stateReqItem.userSOEID = this.userAcctInfoDataService.userAcctInfo.soeid;
+
+    // this.subscriptionData.add(
+    //   this.adminDataService.upsertJobTitle(preEduItem).subscribe({
+    //     next: (response) => {
+    //       this.callParentRefreshData.emit();
+    //       this.forceCloseModal();
+    //     },
+    //     error: (error) => {
+    //       if (error.error && error.error.errMessage) {
+    //         this.errorMessageService.setErrorMessage(error.error.errMessage);
+    //         this.forceCloseModal();
+    //       }
+    //     },
+    //   })
+    // );
+  }
+
+  private fetchStateLicenseItems(): void {
+    this.dropDownDataService.fetchDropdownNumericData('GetLicenseNumericNames', this.selectedLicenseState).subscribe({
+      next: (response) => {
+        this.stateLicenseItems = [{value: 0, label: 'Select'}, ...response];
+        this.xborLicenseRequirementForm.get('licenseId')?.enable(); 
+      },
+      error: (error) => {
+        if (error.error && error.error.errMessage) {
+          this.errorMessageService.setErrorMessage(error.error.errMessage);
+          this.forceCloseModal();
+        }
+      },
+    });
+  }
 
   private forceCloseModal() {
     const modalDiv = document.getElementById('modal-edit-xBor-requirement');
