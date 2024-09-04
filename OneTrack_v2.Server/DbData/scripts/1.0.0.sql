a101a4353f677d778e0ba30782ea9f7d2293d827
ErishMF <erish.faggett.ce@omf.com>
2024-09-04 15:56:56 -0500

    Progress SAVE

--- OneTrack_v2.Server/DbData/scripts/1.0.0.sql
+++ OneTrack_v2.Server/DbData/scripts/1.0.0.sql
@@ -1,14 +1,14 @@
 ﻿USE [License]
 GO
 
-/****** Object:  Table [dbo].[stgADBankerImport]    Script Date: 6/12/2024 5:51:03 PM ******/
+/****** Object:  Table [dbo].[stg_ADBankerImport]    Script Date: 6/12/2024 5:51:03 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[stgADBankerImport](
+CREATE TABLE [dbo].[stg_ADBankerImport](
 	[EmployeeID] [int] NULL,
 	[CourseState] [nvarchar](2) NULL,
 	[StudentName] [nvarchar](50) NULL,
@@ -21,10 +21,10 @@ CREATE TABLE [dbo].[stgADBankerImport](
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[stgADBankerImport] ADD  CONSTRAINT [DF_stg_ADBankerImport_CreateDate]  DEFAULT (getdate()) FOR [CreateDate]
+ALTER TABLE [dbo].[stg_ADBankerImport] ADD  CONSTRAINT [DF_stg_ADBankerImport_CreateDate]  DEFAULT (getdate()) FOR [CreateDate]
 GO
 
-ALTER TABLE [dbo].[stgADBankerImport] ADD  CONSTRAINT [DF_stg_ADBankerImport_IsImportComplete]  DEFAULT ((0)) FOR [IsImportComplete]
+ALTER TABLE [dbo].[stg_ADBankerImport] ADD  CONSTRAINT [DF_stg_ADBankerImport_IsImportComplete]  DEFAULT ((0)) FOR [IsImportComplete]
 GO
 
 
