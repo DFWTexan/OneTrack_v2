61d99428c9f0bb33833a93738b0ff275145d4126
ErishMF <erish.faggett.ce@omf.com>
2024-09-17 12:27:09 -0500

    Update environment.ts

--- onetrack_v2.client/src/app/environments/environment.ts
+++ onetrack_v2.client/src/app/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
     production: false,
-    isDevLoginEnabled: false,
+    isDevLoginEnabled: true,
     isShowEditID: false,
     // LOCAL
     // apiUrl: 'https://localhost:7249/api/',
-    // apiUrl: 'http://localhost:5181/api/', //--> CURRENT USE
+    apiUrl: 'http://localhost:5181/api/', //--> CURRENT USE
     // DEVELOPMENT
-    apiUrl: 'http://ftapid101/OneTrakV2/api/',
+    // apiUrl: 'http://ftapid101/OneTrakV2/api/',
     // QUALITY
     // apiUrl: 'http://ftapiq101/OneTrakV2/api/',
   };
