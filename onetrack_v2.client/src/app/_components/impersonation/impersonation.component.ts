dc5d169fb86e995011aff5616b3dcd8d9bc4c401
ErishMF <erish.faggett.ce@omf.com>
2025-01-28 16:21:40 -0600

    Progress SAVE

--- onetrack_v2.client/src/app/_components/impersonation/impersonation.component.ts
+++ onetrack_v2.client/src/app/_components/impersonation/impersonation.component.ts
@@ -72,12 +72,12 @@ export class ImpersonationComponent implements OnInit, OnDestroy {
     const role = (event.target as HTMLInputElement).value;
     this.impesonatorRole = role;
 
-    (this.userAcctInfo.isAdminRole = role === 'ADMIN' ? true : false),
-      (this.userAcctInfo.isTechRole = role === 'TECH' ? true : false),
-      (this.userAcctInfo.isReadRole = role === 'READ' ? true : false),
-      (this.userAcctInfo.isSuperUser =
-        role === 'DVLPER' ? true : role === null ? true : false),
-      this.userAcctInfoDataService.updateUserAcctInfo(this.userAcctInfo);
+    // (this.userAcctInfo.isAdminRole = role === 'ADMIN' ? true : false),
+    //   (this.userAcctInfo.isTechRole = role === 'TECH' ? true : false),
+    //   (this.userAcctInfo.isReadRole = role === 'READ' ? true : false),
+    //   (this.userAcctInfo.isSuperUser =
+    //     role === 'DVLPER' ? true : role === null ? true : false),
+    //   this.userAcctInfoDataService.updateUserAcctInfo(this.userAcctInfo);
   }
 
   onImpersonate() {
