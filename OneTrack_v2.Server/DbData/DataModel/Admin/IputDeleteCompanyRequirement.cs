f6f72c52a31a7a6b3860a6846bb7ca14651cbbae
ErishMF <erish.faggett.ce@omf.com>
2025-04-22 14:22:49 -0500

    Progress SAVE

--- OneTrack_v2.Server/DbData/DataModel/Admin/IputDeleteCompanyRequirement.cs
+++ OneTrack_v2.Server/DbData/DataModel/Admin/IputDeleteCompanyRequirement.cs
@@ -13,13 +13,13 @@
         public bool LicLevel1 { get; set; }
 
         public bool LicLevel2 { get; set; }
-
+         
         public bool LicLevel3 { get; set; }
 
         public bool LicLevel4 { get; set; }
 
-        // Fixed the issue by replacing 'File' with a more appropriate type, such as 'byte[]' for file data or a custom class.
-        public byte[]? Document { get; set; }
+        // Fixed the issue by removing the invalid instantiation of the IFormFile interface.
+        public IFormFile? Document { get; set; }
 
         public DateTime? StartAfterDate { get; set; }
         public string? UserSOEID { get; set; }
