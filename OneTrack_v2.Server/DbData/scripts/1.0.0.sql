4dbaba9834bd991da3cfd6f4f4795878e6b2b178
ErishMF <erish.faggett.ce@omf.com>
2025-08-19 09:24:15 -0500

    Update 1.0.0.sql

--- OneTrack_v2.Server/DbData/scripts/1.0.0.sql
+++ OneTrack_v2.Server/DbData/scripts/1.0.0.sql
@@ -36,38 +36,3 @@ GO
 ALTER TABLE [dbo].[stg_ADBankerImport] ADD  CONSTRAINT [DF_stg_ADBankerImport_ModifyDate]  DEFAULT (NULL) FOR [ModifyDate]
 GO
 
-INSERT INTO [dbo].[Communications]
-           ([CommunicationName]
-           ,[DocTypeAbv]
-           ,[DocType]
-           ,[DocSubType]
-           ,[DocAppType]
-           ,[EmailAttachments]
-           ,[HasNote]
-           ,[IsActive])
-VALUES
-           ('Licensing Offer Letter'
-           , 'APP'
-           , 'Application'
-           , 'Licensing Offer Letter'
-           , 'Template'
-           , NULL
-           , 0
-           , 1),
-           ('AD Banker Registration Confirmation'
-           , 'APP'
-           , 'Application'
-           , 'AD Banker Registration Confirmation'
-           , 'Template'
-           , NULL
-           , 0
-           , 1),
-           ('Universal Pending License Notice'
-           , 'APP'
-           , 'Application'
-           , 'Universal Pending License'
-           , 'OneTrakEmail'
-           , NULL
-           , 0
-           , 1)
-GO
